78b5ba6227362e5e38a9ca167e8b7669fc6a17de
Guillermo Rodriguez <guillermo@telos.net>
2024-06-07 19:04:40 -0300

    Remove unnecesary utils module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 export * from './txs/index.js';
 export * from './block.js';
-export * from './utils.js';
+
+import {TransactionType, TxOptions} from "@ethereumjs/tx";
+import {
+    TEVMTransaction,
+    TEVMTransactionEIP1559,
+    TEVMTransactionEIP2930,
+    TEVMTransactionEIP4844,
+    TEVMTransactionTypes
+} from "./txs";
+
+export function fromSerializedTEVMData(
+    data: Uint8Array,
+    txOptions: TxOptions = {}
+): TEVMTransactionTypes {
+    if (data[0] <= 0x7f) {
+        // Determine the type.
+        switch (data[0]) {
+            case TransactionType.AccessListEIP2930:
+                return TEVMTransactionEIP2930.fromSerializedTx(data, txOptions)
+            case TransactionType.FeeMarketEIP1559:
+                return TEVMTransactionEIP1559.fromSerializedTx(data, txOptions)
+            case TransactionType.BlobEIP4844:
+                return TEVMTransactionEIP4844.fromSerializedTx(data, txOptions)
+            default:
+                throw new Error(`TypedTransaction with ID ${data[0]} unknown`)
+        }
+    } else {
+        return TEVMTransaction.fromSerializedTx(data, txOptions)
+    }
+}

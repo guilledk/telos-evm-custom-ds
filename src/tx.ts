a629d9348d5165eeec8e6f148f92d8567be22d55
Guillermo Rodriguez <guillermo@telos.net>
2024-06-07 16:24:29 -0300

    Provide EIP1559 tx

--- src/tx.ts
+++ src/tx.ts
@@ -1,13 +1,18 @@
-import {LegacyTransaction, LegacyTxData, TransactionType} from "@ethereumjs/tx";
+import {
+    FeeMarketEIP1559Transaction,
+    FeeMarketEIP1559TxData,
+    LegacyTransaction,
+    LegacyTxData,
+    TransactionType, TxValuesArray,
+} from "@ethereumjs/tx";
 import type { Common } from '@ethereumjs/common';
 
 import type {
-    TxValuesArray as AllTypesTxValuesArray,
     TxOptions,
 } from '@ethereumjs/tx';
 import RLP from "rlp";
+import {bytesToBigInt} from "@ethereumjs/util";
 
-type TxValuesArray = AllTypesTxValuesArray[TransactionType.Legacy]
 
 export class TEVMTransaction extends LegacyTransaction {
 
@@ -34,7 +39,7 @@ export class TEVMTransaction extends LegacyTransaction {
         //     thanks tom! ;)
         // set stream to true to allow for non-zero remainder
         let decoded = RLP.decode(serialized, true);
-        let values: TxValuesArray;
+        let values;
 
         if (!Array.isArray(decoded)) {
             if (decoded.hasOwnProperty('data')) {
@@ -45,7 +50,7 @@ export class TEVMTransaction extends LegacyTransaction {
         } else
             values = decoded;
 
-        return this.fromValuesArray(values as TxValuesArray, opts)
+        return this.fromValuesArray(values, opts)
     }
 
     /**
@@ -53,7 +58,7 @@ export class TEVMTransaction extends LegacyTransaction {
      *
      * Format: `[nonce, gasPrice, gasLimit, to, value, data, v, r, s]`
      */
-    public static fromValuesArray(values: TxValuesArray, opts: TxOptions = {}) {
+    public static fromValuesArray(values: TxValuesArray[TransactionType.Legacy], opts: TxOptions = {}) {
         // If length is not 6, it has length 9. If v/r/s are empty Uint8Arrays, it is still an unsigned transaction
         // This happens if you get the RLP data from `raw()`
         if (values.length !== 6 && values.length !== 9) {
@@ -84,6 +89,117 @@ export class TEVMTransaction extends LegacyTransaction {
                 value,
                 data,
                 v,
+                r,
+                s,
+            },
+            opts
+        )
+    }
+
+    public isSigned(): boolean {
+        const { v, r, s } = this
+        if ((v === undefined || r === undefined || s === undefined)) {
+            return false;
+        } else if ((v == this.common.chainId()) && (r === BigInt(0)) && (s === BigInt(0))) {
+            return false;
+        } else if ((v === BigInt(0)) && (r === BigInt(0)) && (s === BigInt(0))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Validates tx's `v` value
+     */
+    protected _validateTxV(_v?: bigint, common?: Common): Common {
+        return common;
+    }
+
+}
+
+export class TEVMTransactionEIP1559 extends FeeMarketEIP1559Transaction {
+
+    /**
+     * Instantiate a transaction from a data dictionary.
+     *
+     * Format: { nonce, gasPrice, gasLimit, to, value, data, v, r, s }
+     *
+     * Notes:
+     * - All parameters are optional and have some basic default values
+     */
+    public static fromTxData(txData: FeeMarketEIP1559TxData, opts: TxOptions = {}) {
+        return new TEVMTransactionEIP1559(txData, opts)
+    }
+
+    /**
+     * Instantiate a transaction from the serialized tx.
+     *
+     * Format: `rlp([nonce, gasPrice, gasLimit, to, value, data, v, r, s])`
+     */
+    public static fromSerializedTx(serialized: Uint8Array, opts: TxOptions = {}) {
+        // this tx on testnet fails due to having non zero remainder bytes
+        //     https://explorer-test.telos.net/transaction/1fdc14c0b1729533d5a88108dfb1d273dab00e1be4095064e9f65195ffbb14e7
+        //     thanks tom! ;)
+        // set stream to true to allow for non-zero remainder
+        let decoded = RLP.decode(serialized, true);
+        let values;
+
+        if (!Array.isArray(decoded)) {
+            if (decoded.hasOwnProperty('data')) {
+                // @ts-ignore
+                values = decoded.data;
+            } else
+                throw new Error('Invalid serialized tx input. Must be array')
+        } else
+            values = decoded;
+
+        return this.fromValuesArray(values, opts)
+    }
+
+    /**
+     * Create a transaction from a values array.
+     *
+     * Format: `[nonce, gasPrice, gasLimit, to, value, data, v, r, s]`
+     */
+    public static fromValuesArray(values: TxValuesArray[TransactionType.FeeMarketEIP1559], opts: TxOptions = {}) {
+        if (values.length !== 9 && values.length !== 12) {
+            throw new Error(
+                'Invalid EIP-1559 transaction. Only expecting 9 values (for unsigned tx) or 12 values (for signed tx).'
+            )
+        }
+
+        const [
+            chainId,
+            nonce,
+            maxPriorityFeePerGas,
+            maxFeePerGas,
+            gasLimit,
+            to,
+            value,
+            data,
+            accessList,
+            v,
+            r,
+            s,
+        ] = values
+
+        this._validateNotArray({ chainId, v })
+        // TELOS: dont validate leading zeros
+        // validateNoLeadingZeroes({ nonce, maxPriorityFeePerGas, maxFeePerGas, gasLimit, value, v, r, s })
+
+        return new FeeMarketEIP1559Transaction(
+            {
+                chainId: bytesToBigInt(chainId),
+                nonce,
+                maxPriorityFeePerGas,
+                maxFeePerGas,
+                gasLimit,
+                to,
+                value,
+                data,
+                accessList: accessList ?? [],
+                v: v !== undefined ? bytesToBigInt(v) : undefined, // EIP2930 supports v's with value 0 (empty Uint8Array)
                 r,
                 s,
             },
